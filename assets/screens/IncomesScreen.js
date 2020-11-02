fb24a974457d6c2f43d8c7af3edd7d4dea5ac5c8
Ciobanu Andy <andyciobanu17@gmail.com>
2020-11-02 16:49:03 +0200

    v1 first commit

--- assets/screens/IncomesScreen.js
+++ assets/screens/IncomesScreen.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
-
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
 
 function IncomesScreen(props) {
- return (
-  <View style={styles.container}><Text>Incomes screen</Text></View>
- );
+  return (
+    <View style={styles.container}>
+      <Text>Incomesscreen</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{flex:1,justifyContent:"center"}
-})
+  container: { flex: 1, justifyContent: "center" },
+});
 export default IncomesScreen;
