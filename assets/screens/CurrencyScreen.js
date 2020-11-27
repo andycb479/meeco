9f88ab4936a646cf8bda7e34ecbc08edea456a75
Ciobanu Andy <andyciobanu17@gmail.com>
2020-11-27 11:41:06 +0200

    Version 1.0 beta

--- assets/screens/CurrencyScreen.js
+++ assets/screens/CurrencyScreen.js
@@ -1,5 +1,5 @@
-import { MaterialCommunityIcons, FontAwesome5 } from "@expo/vector-icons";
-import React, { useContext, useEffect, useState } from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import React, { useEffect, useState } from "react";
 import { Text, View, StyleSheet, ActivityIndicator } from "react-native";
 import currency from "../api/currency";
 import BackStackNavigation from "../components/BackStackNavigation";
